289e33ca91d7c3df451e3d81517fba52621d3ce9
Pavel Kolesnikov <pkolesnikov@flexaspect.com>
2025-06-23 22:24:48 +0300

    auto migration

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -7,5 +7,8 @@ cd /var/www
 echo "Installing dependencies..."
 composer install --no-interaction
 
+echo "Running DB migrations and seed..."
+php /var/www/src/setup.php
+
 echo "Ready."
 exec apache2-foreground
